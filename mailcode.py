c0e3250ff7281b4d2912078f15151c9debdd13b3
akhil1738 <iamyuva.org@gmail.com>
2024-12-16 15:16:13 +0530

    Update mailcode.py

--- mailcode.py
+++ mailcode.py
@@ -1,17 +1,26 @@
 from flask import Flask
 import smtplib
 from email.mime.text import MIMEText
+from urllib.parse import urlparse
+from selenium import webdriver 
+  
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    driver = webdriver.Chrome() 
+    get_url = driver.current_url 
+    url = get_url
+    parsed_url = urlparse(url)
+    emailid = parsed_url[4]    
         
     subject = "Hello,"
     body = "Hello, This is a test Mail" 
     
     sender = "iamyuva.org@gmail.com"
-    recipients = ["yuvalabs@gmail.com"]
+    recipients = [emailid]
     password = "svkk nskz tvxt wnib"
     
     
