6bdefb7e7d5d284eb6b19a02ee5068477c163756
akhil1738 <iamyuva.org@gmail.com>
2024-12-16 12:26:19 +0530

    Add files via upload

---
+++ mailcode.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+
+subject = "Hello,"
+body = "Hello, This is a test Mail" 
+
+sender = "iamyuva.org@gmail.com"
+recipients = ["yuvalabs@gmail.com"]
+password = "svkk nskz tvxt wnib"
+
+
+def send_email(subject, body, sender, recipients, password):
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(sender, password)
+       smtp_server.sendmail(sender, recipients, msg.as_string())
+    print("Message sent!")
+
+
+send_email(subject, body, sender, recipients, password)
