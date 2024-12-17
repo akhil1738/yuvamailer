3b8f514c65f00efa54da455ad184ebbc385ac577
akhil1738 <iamyuva.org@gmail.com>
2024-12-17 13:10:56 +0530

    Update mailcode.py

--- mailcode.py
+++ mailcode.py
@@ -1,25 +1,32 @@
 from flask import Flask
 import smtplib
 from email.mime.text import MIMEText
-#from urllib.parse import urlparse
-#from selenium import webdriver 
-import requests
-import json
-  
-
+import pyrebase
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   
-    url = "https://yuvalabs.in/"
+    config = {
+      "apiKey": "AIzaSyB1w5Z-mhdwgK2Q_IAl5OfCmFdq9BHZH2I",
+      "authDomain": "savesoil-87f59.firebaseapp.com",
+      "databaseURL": "https://savesoil-87f59-default-rtdb.firebaseio.com",
+      "projectId": "savesoil-87f59",
+      "storageBucket": "savesoil-87f59.firebasestorage.app",
+      "messagingSenderId": "599511712569",
+      "appId": "1:599511712569:web:6c0b343b7d0b4309681ee8",
+      "measurementId": "G-93CJXMS17G"
+    }
 
-    response = json.loads(requests.request("GET", url).text)
+    firebase = pyrebase.initialize_app(config)
 
-    url = response
+    db = firebase.database()
 
-    return url
+    users = db.child("Email").get()
+    email = users.val()
+    
+    return email
 
 if __name__ == '__main__':
     app.run()
